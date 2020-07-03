09dbdf044758f65be5fd7d8999da213e66ac7884
Tyler <tyler.holden@nextgencode.io>
2020-07-03 13:21:31 -0500

    Add confirm button handling

--- src/components/ScheduleMeeting/StartTimeList.tsx
+++ src/components/ScheduleMeeting/StartTimeList.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import StartTimeListItem from './StartTimeListItem';
-import { format } from 'date-fns';
-import {AvailableTimeslot, StartTimeEvent} from './ScheduleMeeting';
+import { StartTimeEvent } from './ScheduleMeeting';
 
 type Props = {
   startTimeListItems?: StartTimeEvent[];
@@ -15,21 +14,69 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   overflow-y: scroll;
+  padding-bottom: 24px;
+  padding-top: 16px;
 `;
 
+const ScrollEdgeFade = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 24px;
+  left: 0;
+  right: 0;
+  z-index: 12;
+  pointer-events: none;
+  &.top {
+    background: linear-gradient(180deg, rgba(255, 255, 255, 1), rgba(0, 0, 0, 0));
+    top: 22px;
+  }
+  &.bottom {
+    bottom: 0;
+    background: linear-gradient(0deg, rgba(255, 255, 255, 1), rgba(0, 0, 0, 0));
+  }
+`;
 
-const ScrollContainer = styled.div`
-  border: 1px solid red;
-  max-height: 100%;
+const ListItemDivider = styled.div<any>`
+  flex-shrink: 0;
   flex: 1;
+  padding: 0.5px;
+  margin: 0px 8px;
+  position: relative;
+  background: ${({ makeTransparent }) => (makeTransparent ? `transparent` : `rgba(0, 0, 0, 0.05)`)};
 `;
 
-const StartTimeList: React.FC<Props> = ({ startTimeListItems =[], selectedDay,  onStartTimeSelect }) => (
-  <Container>
-      {startTimeListItems.map((startTimeEvent: any, i: number) => (
-        <StartTimeListItem startTimeEvent={startTimeEvent} onStartTimeSelect={onStartTimeSelect} key={i} />
-      ))}
-  </Container>
-);
+const StartTimeList: React.FC<Props> = ({ startTimeListItems = [], selectedDay, onStartTimeSelect }) => {
+  const [selectedItemIndex, setSelectedItemIndex] = useState(-1);
+
+  const _onStartTimeSelect = (startTimeEvent: StartTimeEvent, index: number) => {
+    if (selectedItemIndex === index) {
+      onStartTimeSelect(startTimeEvent);
+    } else {
+      setSelectedItemIndex(index);
+    }
+  };
+
+  return (
+    <>
+      <ScrollEdgeFade className="top" />
+      <ScrollEdgeFade className="bottom" />
+      <Container>
+        {startTimeListItems.map((startTimeEvent: any, i: number) => (
+          <React.Fragment key={i}>
+            <StartTimeListItem
+              onCancelClicked={() => setSelectedItemIndex(-1)}
+              selected={i === selectedItemIndex}
+              startTimeEvent={startTimeEvent}
+              onStartTimeSelect={() => _onStartTimeSelect(startTimeEvent, i)}
+            />
+            {i !== startTimeListItems.length - 1 && (
+              <ListItemDivider makeTransparent={selectedItemIndex === i || selectedItemIndex === i + 1} />
+            )}
+          </React.Fragment>
+        ))}
+      </Container>
+    </>
+  );
+};
 
 export default StartTimeList;
--- src/components/ScheduleMeeting/StartTimeListItem.tsx
+++ src/components/ScheduleMeeting/StartTimeListItem.tsx
@@ -1,32 +1,63 @@
 import React from 'react';
 import styled from 'styled-components';
-import { AvailableTimeslot, StartTimeEvent } from './ScheduleMeeting';
+import { StartTimeEvent } from './ScheduleMeeting';
 import { format } from 'date-fns';
 
 type Props = {
-  onStartTimeSelect: (startTimeEvent: StartTimeEvent) => void;
+  onStartTimeSelect: () => void;
   startTimeEvent: StartTimeEvent;
+  selected?: boolean;
+  onCancelClicked: () => void;
 };
 
-const Container = styled.button`
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+`;
+
+const Button = styled.button<any>`
   padding: 16px;
   border: none;
-  background-color: transparent;
+  color: ${({ selected }) => (selected ? `rgb(255, 255, 255)` : `rgb(20,20,20)`)};
+  background-color: ${({ selected }) => (selected ? `rgb(0, 104, 211)` : `rgba(0,0,0,0)`)};
   border-radius: 8px;
   outline: none;
   width: 100%;
-  
+  cursor: pointer;
+
   :hover {
-    background-color: #F0F0F0;
-    cursor: pointer;
-
-  }
-
-  :focus {
-    background-color: #e0e0e0;
+    background-color: ${({ selected }) => (selected ? `rgba(0, 104, 211,.9)` : `rgba(0,0,0,.03)`)};
   }
 `;
 
-const EventListItem: React.FC<Props> = ({ children, onStartTimeSelect, startTimeEvent }) => <Container onClick={() => onStartTimeSelect(startTimeEvent)}>{format(startTimeEvent.startTime, 'h:mmaaaa')}</Container>;
+const CancelButton = styled.button`
+  padding: 8px 24px;
+  border: none;
+  background-color: rgb(0, 0, 0, 0);
+  border-radius: 8px;
+  outline: none;
+  margin-left: 8px;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  :hover {
+    background-color: rgba(0, 0, 0, 0.03);
+  }
+`;
+
+const EventListItem: React.FC<Props> = ({ onStartTimeSelect, startTimeEvent, selected, onCancelClicked }) => {
+  return (
+    <Container>
+      <Button selected={selected} onClick={onStartTimeSelect}>
+        {selected && 'Confirm '}
+        {format(startTimeEvent.startTime, 'h:mmaaaa')}
+      </Button>
+      {selected && <CancelButton onClick={onCancelClicked}>Cancel</CancelButton>}
+    </Container>
+  );
+};
 
 export default EventListItem;
