8eb66c00fa36a4a94594f51f86a8fa7265b03ebb
Tyler <tyler.holden@nextgencode.io>
2020-12-04 11:16:55 -0600

    fix: example fix

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -7,8 +7,8 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 const availableTimeslots = [0, 1, 2, 3, 4, 5].map((id) => {
   return {
     id,
-    startTime: new Date(new Date().setDate(new Date().getDate() + id)).setHours(9, 0, 0, 0),
-    endTime: new Date(new Date().setDate(new Date().getDate() + id)).setHours(17, 0, 0, 0),
+    startTime: new Date(new Date(new Date().setDate(new Date().getDate() + id)).setHours(9, 0, 0, 0)),
+    endTime: new Date(new Date(new Date().setDate(new Date().getDate() + id)).setHours(17, 0, 0, 0)),
   };
 });
 
